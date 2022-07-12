445605ff022a8deb9f7d2d5a981ce2f7b0edff47
ThomasElRubio <thomas.sattelmaier@gmail.com>
2022-07-12 23:02:31 +0200

    Add physical air-drag calculation in y-Direction

--- VR_Gliding/Assets/Scripts/WingsCalculation.cs
+++ VR_Gliding/Assets/Scripts/WingsCalculation.cs
@@ -18,6 +18,11 @@ public class WingsCalculation : MonoBehaviour
     [SerializeField] private GameObject Head;
     [SerializeField] private GameObject Player;
 
+    private Rigidbody rb;
+
+    private float distance;
+    private float dragForce = 0;
+    private float max_wingspan = 3f;
     public float PlyerWeight;
     void Awake()
     {
@@ -27,13 +32,23 @@ public class WingsCalculation : MonoBehaviour
         Core      = GameObject.FindGameObjectWithTag("Core");
         Head      = GameObject.FindGameObjectWithTag("Head");
         Player    = GameObject.FindGameObjectWithTag("Player");
+        rb = Player.GetComponent<Rigidbody>();
+
+        rb.AddForce(500f*rb.mass, 0, 0);
 
         //
     }
 
     void Update()
     {
-        GetHandDistance();
+        
+    }
+
+    void FixedUpdate()
+    {   
+        distance = GetHandDistance();
+        distance = ClampWingspan(distance);
+        dragForce = GravityCalculation(distance, rb.velocity.y);
     }
     
     public float GetHandDistance()
@@ -55,8 +70,35 @@ public class WingsCalculation : MonoBehaviour
         return fullDist;
     }
 
-    public void GravityCalculation()
+    public float GravityCalculation(float wingspan, float falling_velocity)
     {    
+        float area_coefficient = 40f;
+        float density_Air = 1.225f;      
+        float drag_coefficient = 2;         // depends on the object causing the drag. The factor 2 is similar to a rectangle
+
+        // formula to calculate drag which is dependent on the velocity
+        // this allows to simulate a terminal falling velocity
+        float resultingForce = 0.5f * area_coefficient * wingspan * density_Air * drag_coefficient * (falling_velocity * falling_velocity);
+        if (falling_velocity > 0)
+        {
+            resultingForce = -resultingForce;
+        }
+        return resultingForce;
+    }
+
+    public float ClampWingspan(float wingspan)
+    {
+        if(wingspan > max_wingspan)
+        {
+            wingspan = max_wingspan;
+        }
+        if(wingspan < 1.6f)
+        {
+            wingspan = 0.003f;
+        }
+
+        return wingspan/max_wingspan;
+    }
         
 
     }
